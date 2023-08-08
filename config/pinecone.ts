e0f42314abda659b2d1fd4abe52dc03d5f1d9472
libaice <libaice147@gmail.com>
2023-08-08 22:01:53 +0800

    complete ingest data file

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,6 +2,6 @@ if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error("Missing Pinecone index name in .env file");
 }
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? "";
-const PINECONE_NAME_SPACE = "pdf-test";
+const PINECONE_NAME_SPACE = "markmanson-subtle-art";
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,5 +1,43 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from "@/config/pinecone";
+import { pinecone } from "@/utils/pinecone-client";
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+
+const filePath = "docs";
+
 export const run = async () => {
-    console.log("hello world");
+  try {
+    const directoryLoader = new DirectoryLoader(filePath, {
+      ".pdf": (path) => new PDFLoader(path),
+    });
+
+    const rawDocs = await directoryLoader.load();
+
+    const testSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await testSplitter.splitDocuments(rawDocs);
+    console.log("spilt docs ", docs);
+    console.log("creating vector store...");
+
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+
+    console.log(PINECONE_NAME_SPACE);
+    console.log(PINECONE_INDEX_NAME);
+    
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: PINECONE_NAME_SPACE,
+      textKey: "text",
+    });
+  } catch (error) {}
 };
 
 (async () => {
